0543a808b34e73efa22aafbdc4bcd916cb5a62d0
Nithi111 <nithiraj343@gmail.com>
2023-12-25 22:58:51 +0530

    event

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { Jsx } from './components/Jsx'
+import {Event} from './components/Event'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/jsx-components' element={<Jsx/>} />
+      <Route path='/event' element={<Event/>} />
     </Routes>
   )
 }
