3adf94c41b2059b7e9a34ee6e19581157949792a
smartsantosh1928 <smartsantosh1928@gmail.com>
2023-12-25 21:24:50 +0530

    except-router

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import JsxComponent from './components/Jsx'
 import Counter from './components/State'
+import LifeCycle from './components/LifeCycle'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/jsx' element={<JsxComponent/>} />
       <Route path='/state' element={<Counter/>} />
+      <Route path='/life-cycle' element={<LifeCycle/>} />
     </Routes>
   )
 }
